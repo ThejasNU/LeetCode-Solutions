5d062fd42362c4d57a72e4f080d55ebc77643fde
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2022-06-10 19:13:56 +0530

    Time: 2 ms (47.80%), Space: 6.2 MB (56.63%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.cpp
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     string interpret(string command) {
-        string res="";
+        string res;
         for(int i =0;i<command.size();++i){
             if(command[i]=='G'){
-                res=res+"G";
+                res.push_back('G');
             }
             else if(command[i]=='(' && command[i+1]==')'){
-                res=res+'o';
+                res.push_back('o');
             }
             else if(command[i]=='(' && command[i+1]=='a'){
-                res=res+"al";
+                res.push_back('a');
+                res.push_back('l');
                 i=i+2;
             }
             else{
