408a0e2893d1559d5bfdb0800030769545fd7de4
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2022-06-17 23:32:07 +0530

    Time: 560 ms (7.09%), Space: 18.5 MB (36.08%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
@@ -3,10 +3,7 @@ public:
     int removeDuplicates(vector<int>& nums) {
         auto it=nums.begin();
         
-        while(it!=nums.end()){
-            if(it+1==nums.end()){
-                break;
-            }
+        while(it+1!=nums.end()){
             if(*(it)==*(it+1)){
                 nums.erase(it);
             }
