77d1d4f54d74d344854b551bae28936750b598f3
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2022-07-08 20:15:38 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (70.27%) - LeetHub

---
+++ 75-sort-colors/75-sort-colors.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    //Dutch National Flag Algo
+    void sortColors(vector<int>& nums) {
+        int low=0,mid=0,high=nums.size()-1;
+        
+        while(mid<=high){
+            switch(nums[mid]){
+                case 0:
+                    swap(nums[low],nums[mid]);
+                    low++;mid++;
+                    break;
+                case 1:
+                    mid++;
+                    break;
+                case 2:
+                    swap(nums[mid],nums[high]);
+                    high--;
+                    break;
+            }
+        }
+    }
+};
