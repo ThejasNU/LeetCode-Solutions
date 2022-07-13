033616128a18bbee897158774767280bcf8fc44a
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2022-07-13 21:02:49 +0530

    Time: 646 ms (63.17%), Space: 45 MB (95.51%) - LeetHub

---
+++ 493-reverse-pairs/493-reverse-pairs.cpp
@@ -0,0 +1,37 @@
+class Solution {
+private:
+    int count;
+   
+    void checkCount(vector<int>& nums, int start, int mid, int end){
+
+        // two pointers;
+        int l = start, r = mid + 1;
+        while(l <= mid && r <= end){
+            if((long)nums[l] > (long) 2 * nums[r]){
+                count += (mid - l + 1);
+                r++;
+            }else{
+                l++;
+            }
+        }
+        sort(nums.begin() + start, nums.begin() + end + 1);
+        return;
+    }
+    void mergeSort(vector<int>& nums, int start, int end){
+        if(start == end) return;
+        
+        int mid = (start + end)/2;
+        mergeSort(nums,start, mid);
+        mergeSort(nums,mid+1,end);
+        
+        checkCount(nums,start,mid,end);
+        return;
+    }
+public:
+    int reversePairs(vector<int>& nums) {
+        if(!nums.size())return 0;
+        count = 0;
+        mergeSort(nums,0,nums.size()-1);
+        return count;
+    }
+};
