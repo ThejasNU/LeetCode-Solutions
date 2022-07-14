cf7488b2f7b52063ab4748357bf12104b57cd986
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2022-07-14 21:02:46 +0530

    Time: 4 ms (77.53%), Space: 9.6 MB (5.10%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
+    
+    //think that the whole matrix is one single array(see condition), so do binary search on it, to not use extra space, we will create virtual index and row num=virtual_index/m and col num=virtual_index%m
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int lo = 0;
+        if(!matrix.size()) return false;
+        int hi = (matrix.size() * matrix[0].size()) - 1;
         
-        int m=matrix[0].size(),n=matrix.size();
-        int i=0,j=m-1;
-        
-        while(i<n && j>=0){
-            if(matrix[i][j]==target){
+        while(lo <= hi) {
+            int mid = (lo + (hi - lo) / 2);
+            if(matrix[mid/matrix[0].size()][mid % matrix[0].size()] == target) {
                 return true;
             }
-            if(matrix[i][j]>target){
-                j--;
+            if(matrix[mid/matrix[0].size()][mid % matrix[0].size()] < target) {
+                lo = mid + 1;
             }
-            else{
-                i++;
+            else {
+                hi = mid - 1;
             }
         }
-        
         return false;
     }
 };
