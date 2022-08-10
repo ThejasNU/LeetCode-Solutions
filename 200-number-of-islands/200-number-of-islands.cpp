e4306fed1d91eec180e280243e7bc13a6c1e6933
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2022-08-10 19:18:06 +0530

    Time: 42 ms (81.17%), Space: 12.4 MB (55.27%) - LeetHub

---
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    
+    
+    int count=0;
+    
+    void erase(int i,int j,vector<vector<char>>& grid){
+        int n=grid.size();
+        int m=grid[0].size();
+        
+        if(i<0||j<0) return;
+        if(i>=n||j>=m) return;
+        if(grid[i][j]=='0') return;
+        
+        grid[i][j]='0';
+        
+        erase(i-1,j,grid);
+        erase(i+1,j,grid);
+        erase(i,j-1,grid);
+        erase(i,j+1,grid);
+    }
+    
+    int numIslands(vector<vector<char>>& grid) {
+        int count=0;
+        
+        int n=grid.size();
+        int m=grid[0].size();
+        
+        for(int i=0;i<n;++i){
+            for(int j=0;j<m;++j){
+                if(grid[i][j]=='1'){
+                    count++;
+                    erase(i,j,grid);
+                } 
+            }
+        }
+        
+        return count;
+    }
+};
