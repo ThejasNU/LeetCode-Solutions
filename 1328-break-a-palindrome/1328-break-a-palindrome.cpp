8987f8541210e617644da3826eefeb8ece1e02cd
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2022-10-10 06:46:18 +0530

    Time: 5 ms (20.18%), Space: 6.3 MB (45.53%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.cpp
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -3,6 +3,7 @@ public:
     string breakPalindrome(string palindrome) {
         if(palindrome.size()==1) return "";
         
+        //iterating half of the string is enough, if we iterate whole string we won't get answer
         for(int i=0;i<palindrome.size()/2;++i){
             if(palindrome[i]!='a'){
                 palindrome[i]='a';
@@ -10,6 +11,7 @@ public:
             }
         }
         
+        //if all chars till half of the string are a's,then replace last element with b take aba as example or aaaa
         string temp=palindrome.substr(0,palindrome.size()-1);
         temp.push_back('b');
         return temp;
