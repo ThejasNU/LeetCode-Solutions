db481101f8d635a99738f19ba3e3d6cf0c1fe1a0
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2022-10-21 06:23:53 +0530

    Time: 451 ms (23.45%), Space: 72.2 MB (91.47%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    
+    //maintain k+1 elements in set
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        if(k==0 || nums.size()<2) return false;
+        
+        unordered_set<int> s;
+        
+        for(int i=0;i<nums.size();++i){
+            if(i>k){
+                auto it=s.find(nums[i-k-1]);
+                s.erase(it);
+            }
+            
+            if(s.find(nums[i])!=s.end()) return true;
+            else s.insert(nums[i]);
+        }
+        
+        return false;
+    }
+};
