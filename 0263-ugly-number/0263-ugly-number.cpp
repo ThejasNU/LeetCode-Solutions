1afebd62129b8f3fe08a3c8e45a41312ee8c153a
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2022-11-23 19:07:07 +0530

    Time: 0 ms (100.00%), Space: 6 MB (20.08%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+        if(n==1) return true;
+        if(n<=0) return false;
+        
+        while(n%5==0) n/=5;
+        while(n%3==0) n/=3;
+        while(n%2==0) n/=2;
+
+        return n==1;
+
+    }
+};
