a6db0a6371cfe8e2e45907ea5bc26602a2ab41d7
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2022-12-04 12:32:26 +0530

    Time: 1228 ms (5.32%), Space: 78.4 MB (76.81%) - LeetHub

--- 2256-minimum-average-difference/2256-minimum-average-difference.cpp
+++ 2256-minimum-average-difference/2256-minimum-average-difference.cpp
@@ -5,26 +5,25 @@ public:
         
         int n=nums.size();
     
-        vector<long long> suffixSum(n,0);
-        suffixSum[n-1]=nums[n-1];
         long long prefixSum=0;
+        long long totalSum=0;  
         
-        for(int i=n-2;i>=0;--i) suffixSum[i]=nums[i]+suffixSum[i+1];
+        for(int i=0;i<n;++i) totalSum+=nums[i];
         
         long long minn=INT_MAX;
         long long mini=-1;
         long long lAvg,rAvg,res;
+        
         for(int i=0;i<n;++i){
-            lAvg= (prefixSum+nums[i])/(i+1);
-            rAvg=i+1<n?(suffixSum[i+1])/(n-i-1):0;
+            prefixSum+=nums[i];
+            lAvg= (prefixSum)/(i+1);
+            rAvg=i+1<n?(totalSum-prefixSum)/(n-i-1):0;
             res=abs(lAvg-rAvg);
             cout<<res<<endl;
             if(res<minn){
                 minn=res;
                 mini=i;
             }
-            prefixSum+=nums[i];
-            
         }
         
         return mini;
