35d44e8214e7718d315f1d5d785c06ff9e1a8d39
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2022-12-07 11:16:46 +0530

    Time: 1471 ms (15.67%), Space: 141.8 MB (33.10%) - LeetHub

---
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int minScore(int n, vector<vector<int>>& roads) {
+        vector<int> vis(n+1,0);
+        vector<int> adj[n+1];
+        
+        for(auto road:roads){
+            adj[road[0]].push_back(road[1]);
+            adj[road[1]].push_back(road[0]);
+        }
+        
+        dfs(1,adj,vis);
+        
+        int ans=INT_MAX;
+        for(auto road:roads){
+            if(vis[road[0]] || vis[road[1]]) ans=min(ans,road[2]);
+        }
+        
+        return ans;
+    }
+    
+    void dfs(int node,vector<int> adj[],vector<int> &vis){
+        vis[node]=1;
+        
+        for(int v:adj[node]){
+            if(!vis[v]) dfs(v,adj,vis);
+        }
+    }
+    
+
+
+};
