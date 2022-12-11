3a0d7fbbbd3af23f54c7b65acfa6246180eca256
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2022-12-12 03:06:52 +0530

    Time: 706 ms (85.71%), Space: 126.2 MB (100.00%) - LeetHub

---
+++ 2497-maximum-star-sum-of-a-graph/2497-maximum-star-sum-of-a-graph.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int maxStarSum(vector<int>& vals, vector<vector<int>>& edges, int k) {
+        int n=vals.size();
+        vector<int> adj[n];
+        
+        for(int i=0;i<edges.size();++i){
+            adj[edges[i][0]].push_back(edges[i][1]);
+            adj[edges[i][1]].push_back(edges[i][0]);
+        }
+        
+        int ans=INT_MIN;
+        
+        for(int i=0;i<n;++i){
+            priority_queue<int> pq;
+            for(int neighbour:adj[i]){
+                pq.push(vals[neighbour]);
+            }
+            
+            int curSum=vals[i];
+            ans=max(ans,curSum);
+            for(int j=0;j<k && !pq.empty();++j){
+                int cur=pq.top();
+                pq.pop();
+                curSum+=cur;
+                ans=max(ans,curSum);
+            }
+            ans=max(ans,curSum);
+            
+        }
+        
+        return ans;
+    }
+};
