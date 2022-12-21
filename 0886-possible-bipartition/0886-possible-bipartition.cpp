abf3b804bfd0e8b8e3e583bbb0cd5bc2f6639e3e
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2022-12-21 12:47:42 +0530

    Time: 520 ms (24.49%), Space: 70.8 MB (40.71%) - LeetHub

--- 0886-possible-bipartition/0886-possible-bipartition.cpp
+++ 0886-possible-bipartition/0886-possible-bipartition.cpp
@@ -1,43 +1,53 @@
+class UnionFind {
+private:
+    vector<int> parent, rank;
+
+public:
+    UnionFind(int size) {
+        parent.resize(size);
+        rank.resize(size, 0);
+
+        for (int i = 0; i < size; i++) {
+            parent[i] = i;
+        }
+    }
+
+    int find(int x) {
+        if (parent[x] != x) parent[x] = find(parent[x]);
+        return parent[x];
+    }
+
+    void union_set(int x, int y) {
+        int xset = find(x), yset = find(y);
+        if (xset == yset) {
+            return;
+        } else if (rank[xset] < rank[yset]) {
+            parent[xset] = yset;
+        } else if (rank[xset] > rank[yset]) {
+            parent[yset] = xset;
+        } else {
+            parent[yset] = xset;
+            rank[xset]++;
+        }
+    }
+};
+
 class Solution {
 public:
     bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
         vector<int> adj[n];
-        
-        for(auto edge:dislikes){
-            adj[edge[0]-1].push_back(edge[1]-1);
-            adj[edge[1]-1].push_back(edge[0]-1);
+        for (auto dislike : dislikes) {
+            adj[dislike[0]-1].push_back(dislike[1]-1);
+            adj[dislike[1]-1].push_back(dislike[0]-1);
         }
-        
-        vector<int> color(n,-1);
-        
-        for(int i=0;i<n;++i){
-            if(color[i]==-1){
-                if(!bfs(i,adj,color)) return false;
+
+        UnionFind dsu(n);
+        for (int node = 0; node < n; ++node) {
+            for (int neighbor : adj[node]) {
+                if (dsu.find(node) == dsu.find(neighbor)) return false;
+                dsu.union_set(adj[node][0], neighbor);
             }
         }
-        
-        return true;
-    }
-    
-    bool bfs(int src,vector<int> adj[],vector<int> &color){
-        queue<int> q;
-        q.push(src);
-        color[src]=0;
-        
-        while(!q.empty()){
-            int cur=q.front();
-            q.pop();
-            
-            for(int v:adj[cur]){
-                if(color[v]==color[cur]) return false;
-                
-                if(color[v]==-1){
-                    color[v]=1-color[cur];
-                    q.push(v);
-                }
-            }
-        }
-        
         return true;
     }
 };
