728391a39ded19c554f6bc38bb5f74a58da76d8a
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2022-12-30 19:22:11 +0530

    Time: 147 ms (51.51%), Space: 32.3 MB (5.04%) - LeetHub

---
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        vector<vector<int>> matrix(n,vector<int>(n,-1));
+        
+        for(int i=0;i<n;++i){
+            for(int j=0;j<n;++j){
+                if(i==j) matrix[i][j]=0;
+            }
+        }
+        
+        for(auto edge:edges){
+            matrix[edge[0]][edge[1]]=edge[2];
+            matrix[edge[1]][edge[0]]=edge[2];
+        }
+	    
+	    for(int k=0;k<n;++k){
+	        vector<vector<int>> temp=matrix;
+	        for(int i=0;i<n;++i){
+	            if(i==k) continue;
+    	        for(int j=0;j<n;++j){
+    	            if(j==k) continue;
+    	            
+    	            if(matrix[i][j]==-1){
+    	                if(matrix[i][k]!=-1 && matrix[k][j]!=-1){
+    	                    temp[i][j]=temp[i][k]+temp[k][j];
+    	                }
+    	            }
+    	            else{
+    	                if(matrix[i][k]!=-1 && matrix[k][j]!=-1 && matrix[i][j]>matrix[i][k]+matrix[k][j]){
+    	                    temp[i][j]=temp[i][k]+temp[k][j];
+    	                }
+    	            }
+    	        }
+    	    }
+    	    matrix=temp;
+	    }
+        
+        vector<int> count(n,0);
+        int minn=INT_MAX,mini=-1;
+        for(int i=0;i<n;++i){
+            for(int j=0;j<n;++j){
+                if(i==j) continue;
+                
+                if(matrix[i][j]!=-1 && matrix[i][j]<=distanceThreshold) count[i]++;
+            }
+            if(count[i]<=minn){
+                minn=count[i];
+                mini=i;
+            }
+        }
+        
+        return mini;
+    }
+};
