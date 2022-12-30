90da73ecf2eaeca5b46fe96d2b79a3117146fc7b
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2022-12-30 13:18:51 +0530

    Time: 3 ms (99.97%), Space: 10.6 MB (93.18%) - LeetHub

---
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        vector<vector<int>> ans;
+        vector<int> temp;
+        
+        dfs(0,graph,ans,temp);
+        return ans;
+    }
+    
+    void dfs(int node,vector<vector<int>>& graph,vector<vector<int>>& ans,vector<int> &temp){
+        temp.push_back(node);
+        if(node==graph.size()-1){
+            ans.push_back(temp);
+            temp.pop_back();
+            return;
+        }
+        
+        for(int neigh:graph[node]){
+            dfs(neigh,graph,ans,temp);
+        }
+        
+        temp.pop_back();
+    }
+};
