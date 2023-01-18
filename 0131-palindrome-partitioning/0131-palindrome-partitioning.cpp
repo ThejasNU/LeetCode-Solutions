de13ddcbb0f63061d9c5476fcd58840c73e0c686
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2023-01-18 19:32:37 +0530

    Time: 104 ms (91.85%), Space: 49.3 MB (90.00%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> ans;
+        vector<string> cur;
+        
+        solve(s,0,cur,ans);
+        return ans;
+    }
+    
+    void solve(string &s,int start,vector<string> &cur,vector<vector<string>> &ans){
+        if(start>=s.size()){
+            ans.push_back(cur);
+            return;
+        }
+        
+        for(int end=start;end<s.size();++end){
+            if(isPalindrome(s,start,end)){
+                cur.push_back(s.substr(start,end-start+1));
+                solve(s,end+1,cur,ans);
+                cur.pop_back();
+            }
+        }
+    }
+    
+    bool isPalindrome(string &s,int low,int high){
+        while(low<high){
+            if(s[low++]!=s[high--]) return false;
+        }
+        return true;
+    }
+};
