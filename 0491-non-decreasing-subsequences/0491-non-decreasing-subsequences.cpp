ee13340d060997b0bf2fa1ccde63f1bb547eda41
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2023-01-20 13:20:53 +0530

    Time: 519 ms (6.41%), Space: 108.7 MB (6.41%) - LeetHub

--- 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
     vector<vector<int>> findSubsequences(vector<int>& nums) {
-        vector<int> temp;
         set<vector<int>> ans;
-
-        helper(0,nums,temp,ans);
+        int n=(1<<nums.size());
+        
+        for(int bitMask=1;bitMask<n;++bitMask){
+            vector<int> temp;
+            
+            for(int i=0;i<nums.size();++i){
+                if((bitMask>>i)&1) temp.push_back(nums[i]);
+            }
+            
+            bool isIncreasing=true;
+            for(int i=1;i<temp.size();++i){
+                isIncreasing&= temp[i-1]<=temp[i];
+            }
+            
+            if(isIncreasing && temp.size()>1) ans.insert(temp);
+        }
+        
         return vector(ans.begin(),ans.end());
-    }
-    
-    void helper(int ind,vector<int> &nums,vector<int> &temp,set<vector<int>> &ans){
-        if(ind==nums.size()){
-            if(temp.size()>1) ans.insert(temp);
-            return;
-        }
-        
-        //not taking
-        helper(ind+1,nums,temp,ans);
-        
-        //taking
-        if(temp.size()==0 || temp.back()<=nums[ind]){
-            temp.push_back(nums[ind]);
-            helper(ind+1,nums,temp,ans);
-            temp.pop_back();
-        }
-        
-        return;
     }
 };
