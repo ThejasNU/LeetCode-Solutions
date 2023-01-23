7f48eb280594c164d772365998adec9adb2004ad
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2023-01-23 07:32:11 +0530

    Time: 223 ms (55.92%), Space: 68.9 MB (29.65%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -2,15 +2,13 @@ class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
         //indegree-outdegree==n-1 means judge
-        vector<int> count(n+1,0);
+        vector<int> balance(n+1,0);
         for(auto it:trust){
-            count[it[0]]--;
-            count[it[1]]++;
+            balance[it[0]]--;
+            balance[it[1]]++;
         }
         
-        for(int i=1;i<=n;++i){
-            if(count[i]==n-1) return i;
-        }    
+        for(int i=1;i<=n;++i) if(balance[i]==n-1) return i;  
         
         return -1;
     }
