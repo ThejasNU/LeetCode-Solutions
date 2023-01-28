b09970bcddd7e69a784d346b0e315de9039e5151
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2023-01-28 08:21:22 +0530

    Time: 3 ms (58.71%), Space: 6 MB (74.65%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    //hare and tortoise algo to find cycles
+    bool isHappy(int n) {
+        int slow=n;
+        int fast=getNext(n);
+        while(fast!=1 && slow!=fast){
+            slow=getNext(slow);
+            fast=getNext(getNext(fast));
+        }
+        
+        return fast==1;
+    }
+    
+    int getNext(int n){
+        int sum=0;
+        while(n>0){
+            int d=n%10;
+            n/=10;
+            sum+=pow(d,2);
+        }
+        return sum;
+    }
+};
