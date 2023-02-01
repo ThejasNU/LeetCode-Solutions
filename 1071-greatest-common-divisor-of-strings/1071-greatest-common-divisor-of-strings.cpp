84371be63383770c0c04f224234f0a69b3df6633
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2023-02-01 19:04:49 +0530

    Time: 3 ms (83.39%), Space: 7.2 MB (65.71%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -2,6 +2,18 @@ class Solution {
 public:
     string gcdOfStrings(string str1, string str2) {
         int l1=str1.size(),l2=str2.size();
+        if(str1+str2!=str2+str1) return "";
+        
+        int gcdLen=gcd(l1,l2);
+        return str1.substr(0,gcdLen);
+    }
+};
+
+
+class bruteForceSolution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        int l1=str1.size(),l2=str2.size();
         
         for(int i=min(l1,l2);i>=1;--i){
             if(check(str1,str2,i)) return str1.substr(0,i);
