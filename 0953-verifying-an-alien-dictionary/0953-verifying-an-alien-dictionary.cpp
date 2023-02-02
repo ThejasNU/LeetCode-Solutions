9deff56b95199c78f55f840edd1843614efd9c0a
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2023-02-02 07:33:18 +0530

    Time: 3 ms (93.87%), Space: 9.8 MB (25.79%) - LeetHub

---
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isAlienSorted(vector<string>& words, string order) {
+        unordered_map<char,int> m;
+        
+        for(int i=0;i<order.size();++i){
+            m[order[i]]=i;
+        }
+        
+        for(int i=1;i<words.size();++i){
+            for(int j=0;j<words[i-1].size();++j){
+                if(j>=words[i].size()) return false;
+                
+                if(words[i-1][j]!=words[i][j]){
+                    if(m[words[i-1][j]]>m[words[i][j]]) return false;
+                    else break;
+                }
+            }
+        }
+        return true;
+    }
+};
