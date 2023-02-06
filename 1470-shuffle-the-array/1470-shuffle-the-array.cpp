d2b612aed0aee7f2d12622e1c6989b6a9c2ba17b
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2023-02-06 19:09:46 +0530

    Update 1470-shuffle-the-array.cpp

--- 1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
+        //number with 0's followed by 10 1's
+        int allOnes=pow(2,10)-1;
+        //storing 2 numbers last 10 bits is first number and before that 10 bits are second number
+        for(int i=n;i<2*n;++i){
+            int secondNumber=nums[i]<<10;
+            nums[i-n]|=secondNumber;
+        }
+        
+        for(int i=n-1;i>=0;--i){
+            int secondNumber=nums[i]>>10;
+            int firstNumber=nums[i]&allOnes;
+            cout<<firstNumber<<" "<<secondNumber<<endl;
+            nums[2*i+1]=secondNumber;
+            nums[2*i]=firstNumber;
+        }
+        return nums;
+    }
+};
+
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
         vector<int> res;
         
         for(int i=0;i<n;++i){
