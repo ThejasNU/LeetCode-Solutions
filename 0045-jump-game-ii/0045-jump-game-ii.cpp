31a1dd29b3d2531ec562a546a03092ef049337a3
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2023-02-08 06:40:31 +0530

    Time: 490 ms (22.88%), Space: 17.1 MB (43.12%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> dp(n,INT_MAX);
+        dp[n-1]=0;
+        for(int i=n-2;i>=0;--i){
+            int jump=nums[i];
+            for(int j=1;j<=jump;++j){
+                if(i+j<n && dp[i+j]!=INT_MAX) dp[i]=min(dp[i],1+dp[i+j]);
+            }
+        }
+        return dp[0];
+    }
+};
