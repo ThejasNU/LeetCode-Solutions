0db016d78f763df75f04e368942c2cfad5a46b3a
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2023-02-13 06:59:37 +0530

    Time: 3 ms (66.30%), Space: 6 MB (7.66%) - LeetHub

--- 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.cpp
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.cpp
@@ -6,3 +6,14 @@ public:
         return low>high?0:(high-low)/2+1;
     }
 };
+
+class anotherSolution {
+public:
+    int countOdds(int low, int high) {
+        int size=high-low+1;
+        
+        //range can contain odd number of elements only if it starts and end with same parity number
+        if(!(size&1)) return size/2;
+        else return size/2 + high%2;
+    }
+};
