696129d154646ced1445bdb2b697721adcb16616
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2023-02-14 07:23:01 +0530

    Time: 7 ms (24.93%), Space: 7.1 MB (12.88%) - LeetHub

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -4,8 +4,7 @@ public:
         string ans = "";
         
         int carry = 0, i = a.size() - 1, j = b.size() - 1;
-        while(i >= 0 || j >= 0 || carry == 1)
-        {
+        while(i >= 0 || j >= 0 || carry == 1){
             carry += i >= 0 ? a[i--] - '0' : 0;
             carry += j >= 0 ? b[j--] - '0' : 0;
             ans = char(carry % 2 + '0') + ans;
