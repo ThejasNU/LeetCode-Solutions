c04544cdb320f6608c555114d3ef6932716de6f3
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2023-02-20 07:40:05 +0530

    Time: 10 ms (22.58%), Space: 9.7 MB (31.02%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -2,6 +2,27 @@ class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
         int n=nums.size();
+        
+        if(nums[0]>=target) return 0;
+        else if(nums[n-1]<target) return n;
+        
+        int l=0,r=n-1;
+        while(l<r){
+            int mid= l+((r-l)/2);
+            
+            if(nums[mid]==target) return mid;
+            else if(nums[mid]<target) l=mid+1;
+            else r=mid;
+        }
+        return l;
+    }
+};
+
+
+class linearSolution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int n=nums.size();
         int i=0;
         while(i<n && nums[i]<target) ++i;
         return i;
