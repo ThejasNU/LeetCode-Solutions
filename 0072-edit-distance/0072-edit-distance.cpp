764135c2c418f868ce7e4617c903d73244d36908
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2023-02-27 07:32:55 +0530

    Time: 8 ms (87.43%), Space: 9.1 MB (42.81%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -0,0 +1,34 @@
+class Solution
+{
+public:
+    vector<vector<int>> dp;
+    int minDistance(string word1, string word2){
+        dp.resize(word1.length() + 1, vector<int>(word2.length() + 1, -1));
+        return minDistanceRecur(word1, word2, word1.size(), word2.size());
+    }
+
+    int minDistanceRecur(string &word1, string &word2, int word1Index, int word2Index){
+        if (word1Index == 0){
+            return word2Index;
+        }
+        if (word2Index == 0){
+            return word1Index;
+        }
+        if (dp[word1Index][word2Index] != -1){
+            return dp[word1Index][word2Index];
+        }
+        
+        int minEditDistance = 0;
+        if (word1[word1Index - 1] == word2[word2Index - 1]){
+            minEditDistance = minDistanceRecur(word1, word2, word1Index - 1, word2Index - 1);
+        }
+        else{
+            int insertOperation = minDistanceRecur(word1, word2, word1Index, word2Index - 1);
+            int deleteOperation = minDistanceRecur(word1, word2, word1Index - 1, word2Index);
+            int replaceOperation = minDistanceRecur(word1, word2, word1Index - 1, word2Index - 1);
+            minEditDistance = min(insertOperation, min(deleteOperation, replaceOperation)) + 1;
+        }
+        
+        return dp[word1Index][word2Index] = minEditDistance;
+    }
+};
