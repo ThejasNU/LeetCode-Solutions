52a2e7fd3c0ec482e278a38d874fce3acf79acdc
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2023-02-27 18:48:40 +0530

    Time: 19 ms (53.62%), Space: 15.7 MB (77.90%) - LeetHub

--- 0427-construct-quad-tree/0427-construct-quad-tree.cpp
+++ 0427-construct-quad-tree/0427-construct-quad-tree.cpp
@@ -45,6 +45,40 @@ public:
     }
     
     Node* solve(vector<vector<int>>& grid, int x1, int y1, int length) {
+        // Return a leaf node if all values are the same.
+        if (sameValue(grid, x1, y1, length)) {
+            return new Node(grid[x1][y1], true);
+        } else {
+            Node* root = new Node(false, false);
+
+            // Recursive call for the four sub-matrices.
+            root -> topLeft = solve(grid, x1, y1, length / 2);
+            root -> topRight = solve(grid, x1, y1 + length / 2, length / 2);
+            root -> bottomLeft = solve(grid, x1 + length / 2, y1, length / 2);
+            root -> bottomRight = solve(grid, x1 + length / 2, y1 + length / 2, length / 2);
+
+            return root;
+        }
+    }
+    
+    bool sameValue(vector<vector<int>>& grid, int x1, int y1, int length) {
+        for (int i = x1; i < x1 + length; i++) {
+            for (int j = y1; j < y1 + length; j++)
+                if (grid[i][j] != grid[x1][y1])
+                    return false;
+        }
+        return true;
+    }
+};
+
+
+class optimalSolution {
+public:
+    Node* construct(vector<vector<int>>& grid) {
+        return solve(grid, 0, 0, grid.size());
+    }
+    
+    Node* solve(vector<vector<int>>& grid, int x1, int y1, int length) {
         // Return a leaf node if the matrix size is one.
         if (length == 1) {
             return new Node(grid[x1][y1], true);
