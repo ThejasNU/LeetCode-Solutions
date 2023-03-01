54b850cd562b567637114cb248b6124e01ae9de9
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2023-03-01 07:39:38 +0530

    Time: 749 ms (16.44%), Space: 168.1 MB (12.57%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,64 +1,61 @@
 class Solution {
-    // Function to merge two sub-arrays in sorted order.
-    void merge(vector<int> &arr, int left, int mid, int right, vector<int> &tempArr) {
-        // Calculate the start and sizes of two halves.
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        mergeSort(nums, 0, nums.size() - 1);
+        return nums;
+    }
+    
+    void mergeSort(vector<int> &arr, int left, int right) {
+        if (left >= right) {
+            return;
+        }
+        
+        int mid = (left + right) / 2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        
+        merge(arr, left, mid, right);
+    }
+    
+    void merge(vector<int> &arr, int left, int mid, int right) {
         int start1 = left;
         int start2 = mid + 1;
         int n1 = mid - left + 1;
         int n2 = right - mid;
         
-        // Copy elements of both halves into a temporary array.
+        vector<int> v1(n1);
+        vector<int> v2(n2);
+        
         for (int i = 0; i < n1; i++) {
-            tempArr[start1 + i] = arr[start1 + i];
+            v1[i] = arr[start1 + i];
         }
         for (int i = 0; i < n2; i++) {
-            tempArr[start2 + i] = arr[start2 + i];
+            v2[i] = arr[start2 + i];
         }
 
-        // Merge the sub-arrays 'in tempArray' back into the original array 'arr' in sorted order.
         int i = 0, j = 0, k = left;
         while (i < n1 && j < n2) {
-            if (tempArr[start1 + i] <= tempArr[start2 + j]) {
-                arr[k] = tempArr[start1 + i];
-                i += 1;
-            } else {
-                arr[k] = tempArr[start2 + j];
-                j += 1;
+            if (v1[i] <= v2[j]) {
+                arr[k] = v1[i];
+                ++i;
+            } 
+            else {
+                arr[k] = v2[j];
+                ++j;
             }
-            k += 1;
+            ++k;
         }
 
-        // Copy remaining elements
         while (i < n1) {
-            arr[k] = tempArr[start1 + i];
-            i += 1;
-            k += 1;
+            arr[k] = v1[i];
+            ++i;
+            ++k;
         }
         while (j < n2) {
-            arr[k] = tempArr[start2 + j];
-            j += 1;
-            k += 1;
+            arr[k] = v2[j];
+            ++j;
+            ++k;
         }
-    }
-
-    // Recursive function to sort an array using merge sort
-    void mergeSort(vector<int> &arr, int left, int right, vector<int> &tempArr) {
-        if (left >= right) {
-            return;
-        }
-        int mid = (left + right) / 2;
-        // Sort first and second halves recursively.
-        mergeSort(arr, left, mid, tempArr);
-        mergeSort(arr, mid + 1, right, tempArr);
-        // Merge the sorted halves.
-        merge(arr, left, mid, right, tempArr);
-    }
-
-public:
-    vector<int> sortArray(vector<int>& nums) {
-        vector<int> temporaryArray(nums.size());
-        mergeSort(nums, 0, nums.size() - 1, temporaryArray);
-        return nums;
     }
 };
 
