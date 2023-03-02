b503b55e033ff94934726c728492278e81e42942
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2023-03-02 10:21:33 +0530

    Time: 5 ms (68.76%), Space: 9 MB (42.12%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -1,4 +1,30 @@
-class Solution {
+class Solution{
+public:
+    int compress(vector<char>& chars){
+        int n=chars.size();
+        int i=0,res=0;
+        while(i<n){
+            int count=1;
+            while(i+count<n && chars[i+count]==chars[i]) count++;
+            
+            chars[res]=chars[i];
+            ++res;
+            
+            if(count>1){
+                string s=to_string(count);
+                for(char c:s){
+                    chars[res]=c;
+                    ++res;
+                }
+            }
+            i+=count;
+        }
+        return res;
+    }
+};
+
+
+class noobSolution {
 public:
     int compress(vector<char>& chars) {
         int n=chars.size();
