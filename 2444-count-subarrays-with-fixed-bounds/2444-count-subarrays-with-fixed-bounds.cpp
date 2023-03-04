560a99877e1daf51d1825a13cda9e314311cb65f
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2023-03-04 13:28:33 +0530

    Time: 111 ms (88.16%), Space: 70.4 MB (90.20%) - LeetHub

--- 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.cpp
+++ 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.cpp
@@ -4,16 +4,13 @@ public:
         int n=nums.size();        
         long long ans=0;
         int mini=-1,maxi=-1,leftBound=-1;
-        
         for(int i=0;i<n;++i){
             if(nums[i]<minK || nums[i]>maxK) leftBound=i;
-            
             if(nums[i]==minK) mini=i;
             if(nums[i]==maxK) maxi=i;
             
             ans+=max(0,min(maxi,mini)-leftBound);
         }
-        
         return ans;
     }
 };
