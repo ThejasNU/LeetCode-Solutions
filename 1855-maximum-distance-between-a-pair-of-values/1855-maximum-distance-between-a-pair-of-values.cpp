f500feddeccafe411b330c0314a9c17e1223b67c
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2023-03-13 19:45:45 +0530

    Time: 176 ms (90.57%), Space: 98.3 MB (98.49%) - LeetHub

--- 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.cpp
+++ 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.cpp
@@ -3,6 +3,23 @@ public:
     int maxDistance(vector<int>& nums1, vector<int>& nums2) {
         int n1=nums1.size(),n2=nums2.size();
         int maxDist=0;
+        int p1=0,p2=0;
+        while(p1<n1 && p2<n2){
+            if(nums1[p1]>nums2[p2]) ++p1;
+            else{
+                maxDist=max(maxDist,p2-p1);
+                ++p2;
+            }
+        }
+        return maxDist;
+    }
+};
+
+class binarySearchSolution {
+public:
+    int maxDistance(vector<int>& nums1, vector<int>& nums2) {
+        int n1=nums1.size(),n2=nums2.size();
+        int maxDist=0;
         
         int i=0;
         while(i<n1){
