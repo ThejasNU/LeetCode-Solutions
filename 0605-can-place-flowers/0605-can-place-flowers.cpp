935cfe7348008be11c434039bade07a66aa9aba2
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2023-03-20 07:20:28 +0530

    Time: 16 ms (77.89%), Space: 20.3 MB (34.30%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        for(int i=0;i<flowerbed.size();++i){
+            if(flowerbed[i]==1){
+                //skip 2 places coz we can't plant in adjacent plots
+                ++i;
+                continue;
+            }
+            
+            bool emptyLeftPlot=(i==0)||(flowerbed[i-1]==0);
+            bool emptyRightPlot=(i==flowerbed.size()-1)||(flowerbed[i+1]==0);
+            
+            if(emptyLeftPlot && emptyRightPlot){
+                flowerbed[i]=1;
+                --n;
+                if(n<=0) return true;
+                //skip 2 places coz we can't plant in adjacent plots
+                ++i;
+            }
+        }
+        return n<=0;
+    }
+};
