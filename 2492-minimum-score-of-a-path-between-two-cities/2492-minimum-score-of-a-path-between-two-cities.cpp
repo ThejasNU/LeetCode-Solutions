d8f0bf9df819e8efa3465c80615d15c894e518b3
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2023-03-22 09:16:42 +0530

    Update 2492-minimum-score-of-a-path-between-two-cities.cpp

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -3,7 +3,6 @@ public:
     int minScore(int n, vector<vector<int>>& roads) {
         vector<int> vis(n+1,0);
         vector<int> adj[n+1];
-        
         for(auto road:roads){
             adj[road[0]].push_back(road[1]);
             adj[road[1]].push_back(road[0]);
@@ -15,18 +14,13 @@ public:
         for(auto road:roads){
             if(vis[road[0]] || vis[road[1]]) ans=min(ans,road[2]);
         }
-        
         return ans;
     }
     
     void dfs(int node,vector<int> adj[],vector<int> &vis){
         vis[node]=1;
-        
         for(int v:adj[node]){
             if(!vis[v]) dfs(v,adj,vis);
         }
     }
-    
-
-
 };
