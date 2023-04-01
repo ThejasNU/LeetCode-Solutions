0550b3d2df34d9a480d1ea45d97c87997433cadc
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2023-04-01 17:54:27 +0530

    Time: 45 ms (27.23%), Space: 27.6 MB (18.34%) - LeetHub

---
+++ binary-search/binary-search.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l=0,r=nums.size()-1;
+
+        while(l<=r){
+            int mid=(l+r)/2;
+            if(nums[mid]==target) return mid;
+            else if(nums[mid]<target){
+                l=mid+1;
+            }
+            else{
+                r=mid-1;
+            }
+        }
+        return -1;
+    }
+};
