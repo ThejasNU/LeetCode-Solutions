716f84310423311fc55fc6c2b150f57dcac0534a
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2023-04-11 07:25:46 +0530

    Time: 73 ms (92.95%) | Memory: 24.1 MB (94.03%) - LeetSync

--- removing-stars-from-a-string/removing-stars-from-a-string.cpp
+++ removing-stars-from-a-string/removing-stars-from-a-string.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
     string removeStars(string s) {
+        int j=0;
+        int n=s.size();
+        for(int i=0;i<n;++i){
+            if(s[i]=='*') --j;
+            else s[j++]=s[i];
+        }
+        return s.substr(0,j);
+    }
+};
+
+class noobSolution {
+public:
+    string removeStars(string s) {
         int start=0,main=0,i=0;
         int n=s.size();
         
