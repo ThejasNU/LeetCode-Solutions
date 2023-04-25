2309096f1120c5956b75da1ec19e06354b5fc4b4
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2023-04-25 05:55:51 +0530

    Time: 76 ms (88.14%) | Memory: 35.5 MB (77.45%) - LeetSync

--- smallest-number-in-infinite-set/smallest-number-in-infinite-set.cpp
+++ smallest-number-in-infinite-set/smallest-number-in-infinite-set.cpp
@@ -1,10 +1,39 @@
 class SmallestInfiniteSet {
 private:
+    set<int> s;
+    int curInt;
+public:
+    SmallestInfiniteSet() {
+        s.clear();
+        curInt=1;
+    }
+    
+    int popSmallest() {
+        int ans;
+        if(!s.empty()){
+            ans=*s.begin();
+            s.erase(s.begin());
+        }
+        else{
+            ans=curInt;
+            ++curInt;
+        }
+        return ans;
+    }
+    
+    void addBack(int num) {
+        if(curInt<=num || s.find(num)!=s.end()) return;
+        s.insert(num);
+    }
+};
+
+class anotherSmallestInfiniteSet {
+private:
     priority_queue<int,vector<int>,greater<int>> pq;
     unordered_map<int,int> m;
     int curInt;
 public:
-    SmallestInfiniteSet() {
+    anotherSmallestInfiniteSet() {
         pq=priority_queue<int,vector<int>,greater<int>>();
         m.clear();
         curInt=1;
