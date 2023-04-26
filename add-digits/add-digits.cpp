53bf09bdf680f4042bf75895773246fd4485516d
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2023-04-26 18:35:24 +0530

    Time: 0 ms (100.00%) | Memory: 6 MB (63.10%) - LeetSync

--- add-digits/add-digits.cpp
+++ add-digits/add-digits.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
     int addDigits(int num) {
+        int ans=0;
+        while(num>0){
+            ans+=num%10;
+            num/=10;
+            if(num==0 && ans>9){
+                num=ans;
+                ans=0;
+            }
+        }
+        return ans;
+    }
+};
+
+class anotherSolution {
+public:
+    int addDigits(int num) {
         string ans=to_string(num);
         while(ans.size()>1){
             int temp=0;
