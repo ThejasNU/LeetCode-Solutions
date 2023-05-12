6ff98a0005b23ec393ce1eb989e8774258dee113
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2023-05-12 17:07:58 +0530

    Time: 369 ms (72.58%) | Memory: 123 MB (34.83%) - LeetSync

---
+++ solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    long long mostPoints(vector<vector<int>>& questions) {
+       vector<long long> dp(questions.size()+1,-1); 
+       return helper(questions,0,dp);
+    }
+
+    long long helper(vector<vector<int>>& questions,int ind,vector<long long>& dp){
+        if(ind>=questions.size()) return 0;
+        if(dp[ind]!=-1) return dp[ind];
+
+        return dp[ind]=max(questions[ind][0]+helper(questions,ind+questions[ind][1]+1,dp),helper(questions,ind+1,dp));
+    }
+};
