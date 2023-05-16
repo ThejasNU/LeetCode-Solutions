96f94be0893b45a62553794967c9776704a6fe6a
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2023-05-16 18:57:37 +0530

    Time: 72 ms (5.99%) | Memory: 42.5 MB (9.96%) - LeetSync

---
+++ promise-time-limit/promise-time-limit.js
@@ -0,0 +1,21 @@
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit = function(fn, t) {
+	return async function(...args) {
+        return new Promise((resolve,reject)=>{
+            const timeout=setTimeout(()=>{reject("Time Limit Exceeded")},t);
+            fn(...args)
+                .then(resolve)
+                .catch(reject)
+                .finally(()=>clearTimeout(timeout));
+        })
+    }
+};
+
+/**
+ * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+ * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
+ */
