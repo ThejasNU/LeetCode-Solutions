0d91e922c9294178f7d59d9e3df9240a58b2a37b
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2023-05-30 13:41:36 +0530

    Time: 124 ms (41.34%) | Memory: 184.4 MB (14.41%) - LeetSync

---
+++ design-hashset/design-hashset.cpp
@@ -0,0 +1,27 @@
+class MyHashSet {
+public:
+    vector<int> v;
+    MyHashSet() {
+        v.resize(1e6+1,0);
+    }
+    
+    void add(int key) {
+        v[key]=1;
+    }
+    
+    void remove(int key) {
+        v[key]=0;
+    }
+    
+    bool contains(int key) {
+        return v[key]==1;
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
