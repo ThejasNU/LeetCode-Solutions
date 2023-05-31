94d372d402b1a6d994290d4ee4e7b1292b8d2ae8
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2023-05-31 08:49:08 +0530

    Time: 145 ms (91.08%) | Memory: 59.1 MB (44.32%) - LeetSync

--- design-underground-system/design-underground-system.cpp
+++ design-underground-system/design-underground-system.cpp
@@ -1,8 +1,34 @@
 class UndergroundSystem {
 public:
     unordered_map<int,pair<string,int>> cIn;
-    unordered_map<string,vector<int>> timeDif;
+    unordered_map<string,pair<double,int>> timeDif;
     UndergroundSystem() {
+        cIn.clear();
+        timeDif.clear();
+    }
+    
+    void checkIn(int id, string stationName, int t) {
+        cIn[id]={stationName,t};
+    }
+    
+    void checkOut(int id, string stationName, int t) {
+        string res=""+cIn[id].first+"->"+stationName;
+        timeDif[res].first+=t-cIn[id].second;
+        timeDif[res].second+=1;
+    }
+    
+    double getAverageTime(string startStation, string endStation) {
+        string key=startStation+"->"+endStation;
+        pair<double,int> p=timeDif[key];
+        return p.first/p.second;
+    }
+};
+
+class lessOptimizedUndergroundSystem {
+public:
+    unordered_map<int,pair<string,int>> cIn;
+    unordered_map<string,vector<int>> timeDif;
+    lessOptimizedUndergroundSystem() {
         cIn.clear();
         timeDif.clear();
     }
