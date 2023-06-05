c7c351324d92a9dfe4415f1c841c9e72e3beb8c3
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2023-06-05 19:36:19 +0530

    Time: 7 ms (90.62%) | Memory: 10.1 MB (63.27%) - LeetSync

--- check-if-it-is-a-straight-line/check-if-it-is-a-straight-line.cpp
+++ check-if-it-is-a-straight-line/check-if-it-is-a-straight-line.cpp
@@ -4,12 +4,12 @@ public:
         int deltaX=coordinates[0][0]-coordinates[1][0];
         int deltaY=coordinates[0][1]-coordinates[1][1];
         int n=coordinates.size();
-        double x1,x2,y1,y2;
+        int x1,x2,y1,y2;
+        y1=coordinates[0][1];
+        x1=coordinates[0][0];
         for(int i=2;i<n;++i){
             y2=coordinates[i][1];
-            y1=coordinates[0][1];
             x2=coordinates[i][0];
-            x1=coordinates[0][0];
             if((y1-y2)*deltaX!=(x1-x2)*deltaY) return false;
         }
         return true;
