f79216a93d0f29f895928290e0fa267683d7bb1d
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2023-06-06 07:12:37 +0530

    Time: 2 ms (85.80%) | Memory: 8.9 MB (92.44%) - LeetSync

--- can-make-arithmetic-progression-from-sequence/can-make-arithmetic-progression-from-sequence.cpp
+++ can-make-arithmetic-progression-from-sequence/can-make-arithmetic-progression-from-sequence.cpp
@@ -2,6 +2,34 @@ class Solution {
 public:
     bool canMakeArithmeticProgression(vector<int>& arr) {
         int n=arr.size();
+        int maxVal=*max_element(arr.begin(), arr.end());
+        int minVal=*min_element(arr.begin(), arr.end());
+        if((maxVal-minVal)%(n-1)!=0) return false;
+        int dif=(maxVal-minVal)/(n-1);
+        int i=0;
+        while(i<n){
+            if(arr[i]==minVal+i*dif){
+                ++i;
+            }
+            else if((arr[i]-minVal)%dif!=0){
+                return false;
+            }
+            else{
+                int ind=(arr[i]-minVal)/dif;
+                //duplicate elements
+                if(arr[i]==arr[ind]) return false;
+
+                swap(arr[i],arr[ind]);
+            }
+        }
+        return true;
+    }
+};
+
+class sortingSolution {
+public:
+    bool canMakeArithmeticProgression(vector<int>& arr) {
+        int n=arr.size();
         sort(arr.begin(),arr.end());
         int dif=arr[1]-arr[0];
         for(int i=2;i<n;++i){
