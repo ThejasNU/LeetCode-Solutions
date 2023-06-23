d72db246a268c96b4a62168dfb0684a549f70731
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2023-06-23 22:29:05 +0530

    Time: 301 ms (90.41%) | Memory: 141.5 MB (72.59%) - LeetSync

--- longest-arithmetic-subsequence/longest-arithmetic-subsequence.cpp
+++ longest-arithmetic-subsequence/longest-arithmetic-subsequence.cpp
@@ -1,5 +1,25 @@
 class Solution {
 public:
+    //max diff possible is 500 and min diff possible is -500
+    int longestArithSeqLength(vector<int>& nums) {
+        int n=nums.size();
+        int ans=1;
+        int diff;
+        vector<vector<int>> dp(n,vector<int>(1001,1));
+        for(int i=0;i<n;++i){
+            for(int j=0;j<i;++j){
+                //add 500 to avoid negative index
+                diff=500+nums[i]-nums[j];
+                dp[i][diff]=dp[j][diff]+1;
+                ans=max(ans,dp[i][diff]);
+            }
+        }
+        return ans;
+    }
+};
+
+class mapSolution {
+public:
     int longestArithSeqLength(vector<int>& nums) {
         int n=nums.size();
         int ans=1;
@@ -8,7 +28,7 @@ public:
         for(int i=0;i<n;++i){
             for(int j=0;j<i;++j){
                 diff=nums[i]-nums[j];
-                if(dp[j].count(diff)){
+                if(dp[j].find(diff)!=dp[j].end()){
                     dp[i][diff]=dp[j][diff]+1;
                 }
                 else{
