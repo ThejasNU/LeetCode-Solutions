d41de09866812c97d9668f9c14b822996a80c258
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2023-06-28 22:17:15 +0530

    Time: 199 ms (88.29%) | Memory: 65.7 MB (69.53%) - LeetSync

---
+++ path-with-maximum-probability/path-with-maximum-probability.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
+        vector<vector<pair<int, double>>> adj(n);
+        for (int i = 0; i < edges.size(); i++) {
+            int u = edges[i][0];
+            int v = edges[i][1];
+            adj[u].push_back({v, succProb[i]});
+            adj[v].push_back({u, succProb[i]});
+        }
+
+        vector<double> probs(n, 0.0);
+        probs[start] = 1.0;
+        
+        queue<int> q;
+        q.push(start);
+        
+        while (!q.empty()) {
+            int curr = q.front();
+            q.pop();
+            
+            for (auto x : adj[curr]) {
+                int node = x.first;
+                double prob = x.second;
+                double newProb = probs[curr] * prob;
+                
+                if (newProb > probs[node]) {
+                    probs[node] = newProb;
+                    q.push(node);
+                }
+            }
+        }
+        
+        return probs[end];
+    }
+};
