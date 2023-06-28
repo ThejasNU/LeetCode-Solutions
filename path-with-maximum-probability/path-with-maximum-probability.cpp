d5b0df7cb7db9068c1d32aeb3ee3fe458542599f
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2023-06-28 22:23:48 +0530

    Time: 217 ms (57.45%) | Memory: 65.2 MB (71.20%) - LeetSync

--- path-with-maximum-probability/path-with-maximum-probability.cpp
+++ path-with-maximum-probability/path-with-maximum-probability.cpp
@@ -1,6 +1,42 @@
 class Solution {
 public:
     double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
+        unordered_map<int, vector<pair<int, int>>> graph;
+        for (int i = 0; i < edges.size(); i++) {
+            int u = edges[i][0], v = edges[i][1], edgeIdx = i;
+            graph[u].push_back({v, edgeIdx});
+            graph[v].push_back({u, edgeIdx});
+        }
+        
+        vector<double> probabilities(n, 0.0);
+        probabilities[start] = 1.0;
+        
+        priority_queue<pair<double, int>> nodesToExplore;
+        nodesToExplore.push({1.0, start});
+        
+        while (!nodesToExplore.empty()) {
+            auto [prob, u] = nodesToExplore.top();
+            nodesToExplore.pop();
+            
+            if (u == end) return prob;
+            if (probabilities[u] > prob) continue;
+                
+            for (auto [v, edgeIdx] : graph[u]) {
+                double newProb = prob * succProb[edgeIdx];
+                if (newProb > probabilities[v]) {
+                    probabilities[v] = newProb;
+                    nodesToExplore.push({newProb, v});
+                }
+            }
+        }
+        
+        return probabilities[end];
+    }
+};
+
+class altSolution {
+public:
+    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
         vector<vector<pair<int, double>>> adj(n);
         for (int i = 0; i < edges.size(); i++) {
             int u = edges[i][0];
