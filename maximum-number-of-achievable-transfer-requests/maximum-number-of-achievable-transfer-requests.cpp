d61ae55dab0335327931ab371ba861559c0c6459
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2023-07-02 18:44:17 +0530

    Time: 212 ms (42.16%) | Memory: 8.6 MB (100.00%) - LeetSync

--- maximum-number-of-achievable-transfer-requests/maximum-number-of-achievable-transfer-requests.cpp
+++ maximum-number-of-achievable-transfer-requests/maximum-number-of-achievable-transfer-requests.cpp
@@ -1,6 +1,32 @@
 class Solution {
 public:
     int maximumRequests(int n, vector<vector<int>>& requests) {
+        int ans=0;
+        int transfers[n];
+        int lastBitMask=(1<<requests.size());
+        for(int bitmask=0;bitmask<lastBitMask;++bitmask){
+            memset(transfers,0,sizeof(transfers));
+            int numSetBits=__builtin_popcount(bitmask);
+            if(numSetBits<=ans) continue;
+            
+            int cur=bitmask,idx=requests.size()-1;
+            while(cur>0){
+                if(cur&1){
+                    transfers[requests[idx][0]]-=1;
+                    transfers[requests[idx][1]]+=1;
+                }
+                cur>>=1;
+                --idx;
+            }
+            if(count(transfers,transfers+n,0)==n) ans=numSetBits;
+        }
+        return ans;
+    }
+};
+
+class backtrackingSolution {
+public:
+    int maximumRequests(int n, vector<vector<int>>& requests) {
         vector<int> transfers(n,0);
         int ans=helper(n,0,requests,transfers,0);
         return ans==INT_MIN?0:ans;
