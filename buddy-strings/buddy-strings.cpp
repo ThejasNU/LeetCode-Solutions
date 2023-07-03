e9f2543ac2e80c90dbe0a9776485a885894128b6
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2023-07-03 07:26:06 +0530

    Time: 3 ms (78.34%) | Memory: 7.1 MB (19.51%) - LeetSync

--- buddy-strings/buddy-strings.cpp
+++ buddy-strings/buddy-strings.cpp
@@ -1,4 +1,25 @@
-class Solution {
+class Solution{
+public:
+    bool buddyStrings(string s, string goal){
+        if(s.size()!=goal.size()) return false;
+
+        if(s==goal){
+            unordered_set<char> temp(s.begin(),s.end());
+            //check if duplicates are there, if yes return true
+            return temp.size()<s.size();
+        }
+
+        int i=0,j=s.size()-1;
+        while(i<j && s[i]==goal[i]) ++i;
+        while(j>=0 && s[j]==goal[j]) --j;
+
+        if(i<j) swap(s[i],s[j]);
+
+        return s==goal;
+    } 
+};
+
+class altSolution {
 public:
     bool buddyStrings(string s, string goal) {
         if(s.size()!=goal.size()) return false;
