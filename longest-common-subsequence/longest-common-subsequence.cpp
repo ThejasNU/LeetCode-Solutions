80b80e309a4c22cef7aab2a36beda1830d34ee52
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2023-07-04 22:13:55 +0530

    Time: 105 ms (18.78%) | Memory: 10.8 MB (73.50%) - LeetSync

--- longest-common-subsequence/longest-common-subsequence.cpp
+++ longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,32 +1,5 @@
 class Solution {
 public:
-    int dp[1010][1010];
-    
-    int lcs(int i,int j,string &text1,string &text2){
-        if(i<0 || j<0) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        
-        // remove 1 char from text1
-        int ans=lcs(i-1,j,text1,text2);
-        
-        //remove 1 char from text2
-        ans=max(ans,lcs(i,j-1,text1,text2));
-        
-        //remove 1 char from both and compare
-        ans=max(ans,lcs(i-1,j-1,text1,text2)+(text1[i]==text2[j]));
-        
-        return dp[i][j]=ans;
-    }
-    
-    int longestCommonSubsequence(string text1, string text2) {
-        memset(dp,-1,sizeof(dp));
-        return lcs(text1.size()-1,text2.size()-1,text1,text2);
-    }
-};
-
-
-class altSolution {
-public:
     int dp[1001][1001];
     int longestCommonSubsequence(string text1, string text2) {
         memset(dp,-1,sizeof(dp));
