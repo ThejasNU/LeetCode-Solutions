c59adab3fe8a53c2fcba3d77d394744a19e42050
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2023-07-05 19:23:29 +0530

    Time: 43 ms (71.51%) | Memory: 43.9 MB (5.07%) - LeetSync

---
+++ longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int ans;
+    int dp[100001][2];
+    int longestSubarray(vector<int>& nums) {
+        int n=nums.size();
+        ans=0;
+        memset(dp,-1,sizeof(dp));
+        helper(0,nums,0);
+        //if all are 1s, ignore single 1
+        return ans==n?ans-1:ans;
+    }
+
+    int helper(int idx,vector<int>& nums,int deleted){
+        if(idx==nums.size()) return 0;
+        if(dp[idx][deleted]!=-1) return dp[idx][deleted];
+
+        int cur=0;
+        //if a number is already deleted
+        if(deleted){
+            //we will proceed only if num is 1, if 0 return 0 since we can't delete 2 nums
+            if(nums[idx]==1){
+                cur=1+helper(idx+1,nums,1);
+                ans=max(ans,cur);
+            }
+        }
+        //if nothing is deleted from the array
+        else{
+            if(nums[idx]==1){
+                cur=1+helper(idx+1,nums,deleted);
+                ans=max(ans,cur);
+            }
+            else{
+                //ignore this zero, since we haven't deleted anything yet,takes care of the cases where 0s apprear in the beginning or at the end
+                int ignore=helper(idx+1,nums,0);
+                //delete the zero
+                int del=helper(idx+1,nums,1);
+                ans=max(ans,max(ignore,del));
+                //make cur as deleted since that will help the previous recursion calls
+                cur=del;
+            }
+        }
+        return dp[idx][deleted]=cur;
+    }
+};
