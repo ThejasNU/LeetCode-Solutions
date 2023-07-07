7d87b8e02eddb48e6aa9ba69c6f3b97d297f16bf
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2023-07-07 22:28:42 +0530

    Time: 80 ms (19.28%) | Memory: 10 MB (95.61%) - LeetSync

--- maximize-the-confusion-of-an-exam/maximize-the-confusion-of-an-exam.cpp
+++ maximize-the-confusion-of-an-exam/maximize-the-confusion-of-an-exam.cpp
@@ -2,10 +2,31 @@ class Solution {
 public:
     int maxConsecutiveAnswers(string answerKey, int k) {
         int n=answerKey.size();
-        int ans=k;
-        int i=0;
+        int maxSize=0;
         unordered_map<char,int> counter;
         
+        for(int right=0;right<n;++right){
+            counter[answerKey[right]]+=1;
+            int minCount=min(counter['T'],counter['F']);
+            if(minCount<=k){
+                maxSize++;
+            }
+            else{
+                counter[answerKey[right-maxSize]]-=1;
+            }
+        }
+        return maxSize;
+    }
+};
+
+class altSolution {
+public:
+    int maxConsecutiveAnswers(string answerKey, int k) {
+        int n=answerKey.size();
+        int ans=k;
+        unordered_map<char,int> counter;
+        
+        int i=0;
         while(i<n && i<k) counter[answerKey[i++]]+=1;
 
         int left=0;
