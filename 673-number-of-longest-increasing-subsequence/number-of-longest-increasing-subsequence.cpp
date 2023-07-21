c0dfc43f381cbaee8a01fd4e55fb4bbc05ab7b8e
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2023-07-21 22:47:19 +0530

    Time: 200 ms (69.47%) | Memory: 13.1 MB (95.24%) - LeetSync

--- 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.cpp
+++ 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.cpp
@@ -5,6 +5,38 @@ public:
         vector<int> length(n,1);
         vector<int> count(n,1);
 
+        int maxLength=1;
+        for(int i=n-2;i>=0;--i){
+            for(int j=i+1;j<n;++j){
+                if(nums[j]>nums[i]){
+                    if(length[j]+1>length[i]){
+                        length[i]=length[j]+1;
+                        count[i]=count[j];
+                    }
+                    else if(length[j]+1==length[i]){
+                        count[i]+=count[j];
+                    }
+                }
+            }
+            maxLength=max(maxLength,length[i]);
+        }
+
+        int totalCnt=0;
+        for (int i=0;i<n;++i) {
+            if (length[i]==maxLength)
+                totalCnt+=count[i];
+        }
+        return totalCnt;
+    }
+};
+
+class topDownSolution {
+public:
+    int findNumberOfLIS(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> length(n,1);
+        vector<int> count(n,1);
+
         function<void(int)> helper=[&](int idx){
             if(length[idx]!=1) return;
 
