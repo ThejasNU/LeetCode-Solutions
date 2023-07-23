82e6d22e8279390c862f1452e0a914368404bd24
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2023-07-23 21:23:24 +0530

    Time: 5 ms (67.03%) | Memory: 7.2 MB (90.97%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -4,13 +4,12 @@ public:
         int l=0;
         int r=s.size()-1;
         while(l<r){
-            if(!(s[l]>='a' && s[l]<='z') && !(s[l]>='A' && s[l]<='Z') && !(s[l]>='0' && s[l]<='9')){
-                ++l;
-            }
-            else if(!(s[r]>='a' && s[r]<='z') && !(s[r]>='A' && s[r]<='Z') && !(s[r]>='0' && s[r]<='9')){
-                --r;
-            }
-            else if(s[l]==s[r] || toupper(s[l])==s[r] || s[l]==toupper(s[r])){
+            while(l<r && !isalnum(s[l])) ++l;
+            while(l<r && !isalnum(s[r])) --r;
+            
+            if(l>=r) break;
+
+            if(tolower(s[l])==tolower(s[r])){
                 ++l;
                 --r;
             }
