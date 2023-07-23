1697473c0ca71be6b18c4e38242789a789603a76
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2023-07-23 19:22:01 +0530

    Time: 4 ms (95.78%) | Memory: 7.3 MB (39.45%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -2,16 +2,15 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         if(s.size()!=t.size()) return false;
-        int countS[26]={0};
-        int countT[26]={0};
+        int count[26]={0};
 
         for(int i=0;i<s.size();++i){
-            countS[s[i]-'a']+=1;
-            countT[t[i]-'a']+=1;
+            count[s[i]-'a']+=1;
+            count[t[i]-'a']-=1;
         }
 
         for(int i=0;i<26;++i){
-            if(countS[i]!=countT[i]) return false;
+            if(count[i]!=0) return false;
         }
         return true;
     }
