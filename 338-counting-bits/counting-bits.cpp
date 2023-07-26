c37a20a6ffe1fd38cdd849177d09665cb97422eb
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2023-07-27 00:02:18 +0530

    Time: 10 ms (32.95%) | Memory: 7.7 MB (87.10%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -5,6 +5,19 @@ public:
         vector<int> ans(n+1);
         ans[0]=0;
         for(int i=1;i<=n;++i){
+            ans[i]=ans[i>>1]+(i&1);
+        }
+        return ans;
+    }
+};
+
+class readableSolution {
+public:
+    //LOGIC: num*2==num<<1 so the number of bits in ith index==i/2th index
+    vector<int> countBits(int n) {
+        vector<int> ans(n+1);
+        ans[0]=0;
+        for(int i=1;i<=n;++i){
             if(i%2==0) ans[i]=ans[i/2];
             else ans[i]=ans[i-1]+1;
         }
