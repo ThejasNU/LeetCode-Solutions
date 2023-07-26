b86086aacfb49e9af00c1dff4a350e8cb60e6bbd
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2023-07-26 23:51:31 +0530

    Time: 3 ms (79.32%) | Memory: 11 MB (91.29%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n=nums.size();
+        int l=0,r=n-1;
+
+        //make l point to the last element of the first increasing sub array
+        while(l<=r){
+            int mid=(l+r)/2;
+            if(nums[mid]>nums[n-1]){
+                l=mid+1;
+            }
+            else{
+                r=mid-1;
+            }
+        }
+
+        function<int(int,int)> binarySearch=[&](int l,int r){
+            while(l<=r){
+                int mid=(l+r)/2;
+                if(nums[mid]>target){
+                    r=mid-1;
+                }
+                else if(nums[mid]<target){
+                    l=mid+1;
+                }
+                else{
+                    return mid;
+                }
+            }
+            return -1;
+        };
+
+
+        int index=binarySearch(0,l-1);
+        if(index!=-1) return index;
+
+        return binarySearch(l,n-1);
+    }
+};
