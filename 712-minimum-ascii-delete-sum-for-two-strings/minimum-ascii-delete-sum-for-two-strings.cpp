ca558b2332d3c246b4b63e86881956d2da60c3ff
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2023-07-31 22:58:57 +0530

    Time: 75 ms (28.26%) | Memory: 15.4 MB (14.89%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
@@ -4,15 +4,19 @@ public:
         int n1=s1.size();
         int n2=s2.size();
         vector<vector<int>> dp(n1+1,vector<int>(n2+1,-1));
+        
         dp[n1][n2]=0;
+        
         int i=n1;
         for(int j=n2-1;j>=0;--j){
             dp[i][j]=dp[i][j+1]+int(s2[j]);
         }
+        
         int j=n2;
         for(int i=n1-1;i>=0;--i){
             dp[i][j]=dp[i+1][j]+int(s1[i]);
         }
+        
         function<int(int,int)> helper=[&](int i,int j)->int{
             if(dp[i][j]!=-1) return dp[i][j];
 
@@ -23,6 +27,7 @@ public:
                 return dp[i][j]=min(skip1,skip2);
             }
         };
+        
         return helper(0,0);
     }
 };
