8705271f2bc59a3eed2f41017cf6f393a77ab2a8
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2023-08-01 09:59:31 +0530

    Time: 11 ms (92.70%) | Memory: 10.4 MB (41.84%) - LeetSync

--- 77-combinations/combinations.cpp
+++ 77-combinations/combinations.cpp
@@ -2,6 +2,27 @@ class Solution {
 public:
     vector<vector<int>> combine(int n, int k) {
         vector<vector<int>> ans;
+        int limit=(1<<n)-1;
+
+        for(int bitMask=0;bitMask<=limit;++bitMask){
+            int bitCount=__builtin_popcount(bitMask);
+            if(bitCount!=k) continue;
+
+            vector<int> temp;
+            for(int i=0;i<n;++i){
+                if(((bitMask>>i)&1)==1) temp.push_back(i+1);
+            }
+            ans.push_back(temp);
+        }
+
+        return ans;
+    }
+};
+
+class backtrackingSolution {
+public:
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> ans;
         vector<int> temp;
         function<void(int)> helper=[&](int num)->void{
             if(num==n+1){
