e23807edb4c181e74a96a47b94f1afcdb6f7a84f
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2023-08-01 23:55:17 +0530

    Time: 0 ms (100.00%) | Memory: 6 MB (22.93%) - LeetSync

--- 190-reverse-bits/reverse-bits.cpp
+++ 190-reverse-bits/reverse-bits.cpp
@@ -3,6 +3,19 @@ public:
     uint32_t reverseBits(uint32_t n) {
         uint32_t ans=0;
         for(int i=0;i<32;++i){
+            ans<<=1;
+            ans|=(n&1);
+            n>>=1;
+        }
+        return ans;
+    }
+};
+
+class altSolution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t ans=0;
+        for(int i=0;i<32;++i){
             if(((n>>i)&1)==1){
                 ans|=(1<<(31-i));
             }
