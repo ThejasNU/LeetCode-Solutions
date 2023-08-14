b54af09e5689b86fddd9b71d484dc4dec9c935da
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2023-08-14 19:45:39 +0530

    Time: 0 ms (100.00%) | Memory: 6.2 MB (72.79%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,21 +1,3 @@
-// class Solution { // 12 ms, faster than 9.19%
-// public:
-//     int memo[100] = {};
-//     int numDecodings(const string& s) {
-//         return dp(s, 0);
-//     }
-//     int dp(const string& s, int i) {
-//         if (i == s.size()) return 1;
-//         if (memo[i] != 0) return memo[i];
-//         int ans = 0;
-//         if (s[i] != '0') // Single digit
-//             ans += dp(s, i+1);
-//         if (i+1 < s.size() && (s[i] == '1' || s[i] == '2' && s[i+1] <= '6')) // Two digits
-//             ans += dp(s, i+2);
-//         return memo[i] = ans;
-//     }
-// };
-
 class Solution {
 public:
     int numDecodings(string s) {
@@ -29,8 +11,10 @@ public:
 
         int ways=0;
         if(s[idx]!='0'){
+            //one digit number
             ways+=helper(idx+1,s,dp);
 
+            //two digits number
             if(idx+1<s.size()){
                 int twoDigitNum=stoi(s.substr(idx,2));
                 if(twoDigitNum>=0 && twoDigitNum<=26){
