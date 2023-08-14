3e3fde0c7095069ae6e9856d2058dbcb75a38a44
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2023-08-14 19:54:19 +0530

    Time: 4 ms (31.77%) | Memory: 6.1 MB (23.88%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.cpp
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -2,6 +2,18 @@ class Solution {
 public:
     int hammingWeight(uint32_t n) {
         int count=0;
+        while(n>0){
+            n&=(n-1);
+            count+=1;
+        }
+        return count;
+    }
+};
+
+class altSolution {
+public:
+    int hammingWeight(uint32_t n) {
+        int count=0;
         for(int i=0;i<32;++i){
             if((n&1)==1) count+=1;
             n>>=1;
