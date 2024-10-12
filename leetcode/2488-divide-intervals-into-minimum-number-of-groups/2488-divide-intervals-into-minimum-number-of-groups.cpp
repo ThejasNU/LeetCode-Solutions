68f9097f8d01e069d2e92768efde97991e61884b
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2024-10-12 08:21:26 +0530

    Time: 256 ms (85.14%), Space: 214.6 MB (14.41%) - LeetHub

--- leetcode/2488-divide-intervals-into-minimum-number-of-groups/2488-divide-intervals-into-minimum-number-of-groups.cpp
+++ leetcode/2488-divide-intervals-into-minimum-number-of-groups/2488-divide-intervals-into-minimum-number-of-groups.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     int minGroups(vector<vector<int>>& intervals) {
-        int start=INT_MAX,end=INT_MIN;
-        for(auto& interval:intervals){
-            start=min(start,interval[0]);
-            end=max(end,interval[1]);
+        int start = INT_MAX, end = INT_MIN;
+        for (auto& interval : intervals) {
+            start = min(start, interval[0]);
+            end = max(end, interval[1]);
         }
 
-        vector<int> count(end+2,0);
-        for(auto& interval:intervals){
-            count[interval[0]]+=1;
-            count[interval[1]+1]-=1;
+        vector<int> count(end + 2, 0);
+        for (auto& interval : intervals) {
+            count[interval[0]] += 1;
+            count[interval[1] + 1] -= 1;
         }
 
-        int cur=0;
-        int ans=0;
-        for(int i=start;i<=end;++i){
-            cur+=count[i];
-            ans=max(ans,cur);
+        int cur = 0;
+        int ans = 0;
+        for (int i = start; i <= end; ++i) {
+            cur += count[i];
+            ans = max(ans, cur);
         }
 
         return ans;
