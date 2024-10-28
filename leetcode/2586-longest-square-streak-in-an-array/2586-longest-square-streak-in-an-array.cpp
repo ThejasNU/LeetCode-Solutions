5ef4e9d006b29ab2714572d181d9e936c8bae93d
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2024-10-28 09:55:32 +0530

    Time: 116 ms (31.11%), Space: 108.9 MB (63.34%) - LeetHub

--- leetcode/2586-longest-square-streak-in-an-array/2586-longest-square-streak-in-an-array.cpp
+++ leetcode/2586-longest-square-streak-in-an-array/2586-longest-square-streak-in-an-array.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
-    
     int longestSquareStreak(vector<int>& nums) {
-        unordered_map<int,int> dp;
-        sort(nums.begin(),nums.end());
-        int ans=0;
-        
+        unordered_map<int, int> dp;
+        sort(nums.begin(), nums.end());
+        int ans = 0;
+
         int temp;
-        for(int n:nums){
-            temp=sqrt(n);
-            
-            if(temp*temp==n){
-                dp[n]=1+dp[temp];
+        for (int n : nums) {
+            temp = sqrt(n);
+
+            if (temp * temp == n) {
+                dp[n] = 1 + dp[temp];
+            } else {
+                dp[n] = 1;
             }
-            else{
-                dp[n]=1;
-            }
-            
-            ans=max(ans,dp[n]);
+
+            ans = max(ans, dp[n]);
         }
-        
-        return ans<2?-1:ans;
+
+        return ans < 2 ? -1 : ans;
     }
 };
