c8233ba4018cc843db39f9700a806a7385eb6b5b
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2024-11-18 09:38:33 +0530

    Time: 2 ms (16.95%), Space: 10.3 MB (84%) - LeetHub

--- leetcode/1755-defuse-the-bomb/1755-defuse-the-bomb.cpp
+++ leetcode/1755-defuse-the-bomb/1755-defuse-the-bomb.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
     vector<int> decrypt(vector<int>& code, int k) {
-        int n=code.size();
-        vector<int> ans(n,0);
+        int n = code.size();
+        vector<int> ans(n, 0);
 
-        if(k==0){
+        if (k == 0) {
             return ans;
         }
 
-        int start=1, end=k, sum=0;
-        if(k<0){
-            start=n-abs(k);
-            end=n-1;
+        int start = 1, end = k, sum = 0;
+        if (k < 0) {
+            start = n - abs(k);
+            end = n - 1;
         }
 
-        for(int i=start;i<=end;++i){
-            sum+=code[i];
+        for (int i = start; i <= end; ++i) {
+            sum += code[i];
         }
 
-        for(int i=0;i<n;++i){
-            ans[i]=sum;
-            sum-=code[start%n];
+        for (int i = 0; i < n; ++i) {
+            ans[i] = sum;
+            sum -= code[start % n];
             ++start;
             ++end;
-            sum+=code[end%n];
+            sum += code[end % n];
         }
 
         return ans;
