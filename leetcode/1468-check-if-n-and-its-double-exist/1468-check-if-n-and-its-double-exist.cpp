be8f9b3a2c8df66c4651ede20ebdfe01b0c3db7f
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2024-12-01 05:46:50 +0530

    Time: 0 ms (100%), Space: 13.9 MB (15.78%) - LeetHub

---
+++ leetcode/1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        unordered_set<int> st;
+        for(int num:arr){
+            if(st.find(num*2)!=st.end() || (num%2==0 && st.find(num/2)!=st.end())){
+                return true;
+            }
+            st.insert(num);
+        }
+        return false;
+    }
+};
