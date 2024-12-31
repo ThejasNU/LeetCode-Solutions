fba8e59fb43f42b184f06f09327d077019fd789e
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2024-12-31 07:15:26 +0530

    Time: 4 ms (15.64%), Space: 12.6 MB (69.51%) - LeetHub

---
+++ leetcode/1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int dp[1010];
+
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+        memset(dp, -1, sizeof(dp));
+        return helper(days, costs, 0);
+    }
+
+    int helper(vector<int>& days, vector<int>& costs, int idx) {
+        int n = days.size();
+
+        if (idx >= n) {
+            return 0;
+        }
+
+        if (dp[idx] != -1) {
+            return dp[idx];
+        }
+
+        int option1 = costs[0] + helper(days, costs, idx + 1);
+
+        int i = idx;
+        while (i < n && days[i] < days[idx] + 7) {
+            ++i;
+        }
+        int option2 = costs[1] + helper(days, costs, i);
+
+        i = idx;
+        while (i < n && days[i] < days[idx] + 30) {
+            ++i;
+        }
+        int option3 = costs[2] + helper(days, costs, i);
+
+        return dp[idx] = min(option1, min(option2, option3));
+    }
+};
