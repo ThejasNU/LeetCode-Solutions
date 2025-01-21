df711ad10b22bb8f8553348210805e56caa6f776
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2025-01-21 09:59:33 +0530

    Time: 0 ms (100%), Space: 66.8 MB (53.39%) - LeetHub

---
+++ leetcode/2145-grid-game/2145-grid-game.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    long long gridGame(vector<vector<int>>& grid) {
+        long long firstRowSum = accumulate(begin(grid[0]), end(grid[0]), 0LL),
+                  secondRowSum = 0;
+        long long minimumSum = LONG_LONG_MAX;
+        for (int turnIndex = 0; turnIndex < grid[0].size(); ++turnIndex) {
+            firstRowSum -= grid[0][turnIndex];
+            minimumSum = min(minimumSum, max(firstRowSum, secondRowSum));
+            secondRowSum += grid[1][turnIndex];
+        }
+        return minimumSum;
+    }
+};
