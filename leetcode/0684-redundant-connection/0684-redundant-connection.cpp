42e2ca6d3fd22fb7f5fa589a1276fbb1015f1dff
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2025-01-29 07:44:23 +0530

    Time: 0 ms (100%), Space: 13 MB (69.21%) - LeetHub

--- leetcode/0684-redundant-connection/0684-redundant-connection.cpp
+++ leetcode/0684-redundant-connection/0684-redundant-connection.cpp
@@ -6,15 +6,15 @@ public:
     DisjointSet(int n) {
         rank.resize(n, 0);
         parent.resize(n);
-        for(int i = 0;i<n;i++) {
+        for (int i = 0; i < n; i++) {
             parent[i] = i;
         }
     }
 
     int find(int node) {
-        if(node == parent[node]){
+        if (node == parent[node]) {
             return node;
-        } 
+        }
 
         return parent[node] = find(parent[node]);
     }
@@ -22,17 +22,15 @@ public:
     bool unionBR(int u, int v) {
         int ulp_u = find(u);
         int ulp_v = find(v);
-        if(ulp_u == ulp_v){
+        if (ulp_u == ulp_v) {
             return false;
         }
-        
-        if(rank[ulp_u] < rank[ulp_v]) {
+
+        if (rank[ulp_u] < rank[ulp_v]) {
             parent[ulp_u] = ulp_v;
-        }
-        else if(rank[ulp_v] < rank[ulp_u]) {
+        } else if (rank[ulp_v] < rank[ulp_u]) {
             parent[ulp_v] = ulp_u;
-        }
-        else {
+        } else {
             parent[ulp_v] = ulp_u;
             rank[ulp_u]++;
         }
@@ -44,11 +42,11 @@ public:
 class Solution {
 public:
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-        int n=edges.size();
-        
+        int n = edges.size();
+
         DisjointSet dsu(n);
-        for(vector<int>& edge:edges){
-            if(!dsu.unionBR(edge[0]-1,edge[1]-1)){
+        for (vector<int>& edge : edges) {
+            if (!dsu.unionBR(edge[0] - 1, edge[1] - 1)) {
                 return edge;
             }
         }
