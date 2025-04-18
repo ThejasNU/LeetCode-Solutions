039f525f62f0176505f2b651517fa8cd6caf818e
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2025-04-18 08:44:27 +0530

    Time: 0 ms (100%), Space: 8.6 MB (99.04%) - LeetHub

---
+++ leetcode/0038-count-and-say/0038-count-and-say.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+        string res = "1";
+        while (--n) {
+            string s = "";
+            for (int i = 0; i < res.size(); i++) {
+                int count = 1;
+                while (i + 1 < res.size() && res[i] == res[i + 1]) {
+                    ++count;
+                    i++;
+                }
+                s.push_back(count + '0');
+                s.push_back(res[i]);
+            }
+            res = s;
+        }
+        return res;
+    }
+};
